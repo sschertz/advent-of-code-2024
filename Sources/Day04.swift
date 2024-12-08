ae88db85762da72cb7a7b26f58670ca16855983b
Sara Schertz <sschertz@me.com>
2024-12-08 15:25:54 -0700

    Setup for Day 6 and move Array2D into project
    So also fixed Day 4 to use the shared class
    Renamed Array2D to  Grid

--- Sources/Day04.swift
+++ Sources/Day04.swift
@@ -9,9 +9,9 @@ struct Day04: AdventDay {
   }
   
   func makeGrid(letterToSave: Character) ->
-  (grid: Array2D<Character>, savedCoords: [(c:Int, r:Int)]){
+  (grid: Grid<Character>, savedCoords: [(c:Int, r:Int)]){
     
-    var grid:Array2D<Character> = Array2D(columns: strings[0].count, rows: strings.count, initialValue: ".")
+    var grid:Grid<Character> = Grid(columns: strings[0].count, rows: strings.count, initialValue: ".")
     var row=0
     var coordsToSave:[(c:Int,r:Int)] = []
     
@@ -40,7 +40,7 @@ struct Day04: AdventDay {
     
     for coord in startingChars{
       // check all 8 directions for valid words
-      for direction in Array2D<Character>.Direction.allCases{
+      for direction in Grid<Character>.Direction.allCases{
         // print("check \(direction)")
         guard grid.isValidPath(from: coord, direction: direction, distance: word.count-1) else {
           continue
@@ -78,7 +78,7 @@ struct Day04: AdventDay {
     let (grid,startingChars) = makeGrid(letterToSave: word.first!)
     let middleCoordDistance = 1 //Ideally this should be calculated
     var middleCoordinates: [(c:Int, r:Int)] = []
-    let diagonalDirections = Array2D<Character>.Direction.diagonalDirections()
+    let diagonalDirections = Grid<Character>.Direction.diagonalDirections()
     
     for coord in startingChars{
       // Check just the diagonal directions for words -- horiz and vertical
@@ -134,95 +134,3 @@ struct Day04: AdventDay {
   }
 }
 
-public struct Array2D<T> {
-  public let columns: Int
-  public let rows: Int
-  fileprivate var array: [T]
-  
-  public init(columns: Int, rows: Int, initialValue: T) {
-    self.columns = columns
-    self.rows = rows
-    array = .init(repeating: initialValue, count: rows*columns)
-  }
-  
-  public func isValidCoordinate(_ column: Int, _ row: Int) -> Bool {
-    row >= 0 && row < rows && column >= 0 && column < columns
-  }
-
-  public func isValidPath(from start: (c: Int,r: Int),
-                          direction: Direction,
-                          distance: Int=1) -> Bool{
-    
-    let newCoord = direction.getNextCoordinate(from: start, by: distance)
-    return isValidCoordinate(newCoord.c, newCoord.r)
-  }
-  
-  public func move(_ direction:Direction,
-                   from start:(c: Int, r:Int),
-                   by distance: Int=1) -> (c: Int, r: Int)? {
-    
-    let newCoord = direction.getNextCoordinate(from: start, by: distance)
-    if isValidCoordinate(newCoord.c, newCoord.r){
-      return newCoord
-    } else { return nil }
-  }
-  
-  public func prettyPrint() {
-    for i in 0..<self.rows {
-      print("[", terminator: "")
-      for j in 0..<self.columns {
-        if j == self.columns - 1 {
-          print("\(self[j, i])", terminator: "")
-        } else {
-          print("\(self[j, i]) ", terminator: "")
-        }
-      }
-      print("]")
-    }
-  }
-  
-  public subscript(column: Int, row: Int) -> T {
-    get {
-      precondition(column < columns, "Column \(column) Index is out of range. Array<T>(columns: \(columns), rows:\(rows))")
-      precondition(row < rows, "Row \(row) Index is out of range. Array<T>(columns: \(columns), rows:\(rows))")
-      return array[row*columns + column]
-    }
-    set {
-      precondition(column < columns, "Column \(column) Index is out of range. Array<T>(columns: \(columns), rows:\(rows))")
-      precondition(row < rows, "Row \(row) Index is out of range. Array<T>(columns: \(columns), rows:\(rows))")
-      array[row*columns + column] = newValue
-    }
-  }
-  
-  public enum Direction: CaseIterable {
-    case up, down, left, right, upLeft, upRight, downLeft, downRight
-    
-    func getNextCoordinate(from start:(c: Int, r:Int), by distance: Int=1) -> (c: Int, r: Int){
-      let (c, r) = start
-      
-      switch self {
-      case .up:
-        return (c: c, r: r-distance)
-      case .down:
-        return (c:c, r: r+distance)
-      case .left:
-        return (c: c-distance, r: r)
-      case .right:
-        return (c: c+distance, r: r)
-      case .upLeft:
-        return (c: c-distance, r: r-distance)
-      case .upRight:
-        return (c: c+distance, r: r-distance)
-      case .downLeft:
-        return (c: c-distance, r: r+distance)
-      case .downRight:
-        return (c: c+distance, r: r+distance)
-      }
-    }
-    
-    static func diagonalDirections() -> [Direction] {
-      return [.upLeft, .upRight, .downLeft, .downRight]
-      
-    }
-  }
-}
