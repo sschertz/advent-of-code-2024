9d1ad4a2032bb3ea29f5c105d65ad6757e1ac339
Sara Schertz <sschertz@me.com>
2024-12-14 13:04:22 -0700

    Second attempt (still to slow)
    Separated into two passes, so we don't do the pass with || while
    processing the equations that work with just + or *
    
    But, still too slow.

--- Sources/Day07.swift
+++ Sources/Day07.swift
@@ -29,6 +29,22 @@ struct Day07: AdventDay {
       + Array(repeating: "*", count: numberOperators)
       + Array(repeating: "|", count: numberOperators)
     }
+    
+    var basicOperatorCombos: [[Character]] {
+      operators.uniquePermutations(ofCount:numberOperators).map{Array($0)}
+    }
+    
+    var concatOperatorCombos: [[Character]]{
+      let basicOperatorCombos = Set(basicOperatorCombos)
+      let all = Set(operatorsWithConcat.uniquePermutations(ofCount: numberOperators).map{Array($0)})
+      return Array(all.subtracting(basicOperatorCombos))
+    }
+    
+    // might nto need this anymore?
+    func getOperatorPermutations(includeAll: Bool = false) -> [[Character]]{
+      let operators = (includeAll ? self.operatorsWithConcat : self.operators)
+      return operators.uniquePermutations(ofCount:numberOperators).map{Array($0)}
+    }
   }
 
   func getOperator (_ input: Character) -> (Int,Int) -> Int {
@@ -38,6 +54,35 @@ struct Day07: AdventDay {
     } else { return (||) }
   }
   
+  func processEquations (_ equations:[Equation], includeAll: Bool = false) -> (Int,[Equation]) {
+    var failedEquations: [Equation] = []
+    
+    let result = equations.compactMap { equation in
+      var values = equation.values
+      print("solving \(equation.result)")
+      
+      let combinationsToTry: [[Character]] =
+      includeAll ? equation.concatOperatorCombos : equation.basicOperatorCombos
+      
+      let runningTotal = values.removeFirst()
+      
+      for comboToTest in combinationsToTry {
+        let testResult = values.enumerated().reduce(runningTotal) { partialResult, nextItem in
+          //partialResult is the sum so far. nextItem is the next value
+          let (operatorIndex,value) = nextItem
+          let operation = getOperator(comboToTest[operatorIndex])
+          return operation(partialResult,value)
+        }
+        if testResult == equation.result { return equation.result }
+      }
+      failedEquations.append(equation)
+      return nil
+    }.reduce(0, + )
+    
+    return (result,failedEquations)
+    
+  }
+
   
   func part1() -> Any {
     let equations = entities
@@ -67,27 +112,14 @@ struct Day07: AdventDay {
   func part2() -> Any {
     
     let equations = entities
-    return equations.compactMap { equation in
-      let result = equation.result
-      var values = equation.values
-      let combinationsToTry = equation.operatorsWithConcat.uniquePermutations(ofCount: equation.numberOperators).map{Array($0)}
-      // print("Find operators to get \(result) from \(values)")
-      
-      // Get the first number to start the reduce
-      let runningTotal = values.removeFirst()
-      
-      // Loop all the combos, bail out when we find a match
-      for comboToTest in combinationsToTry {
-        let testResult = values.enumerated().reduce(runningTotal) { partialResult, nextItem in
-          //partialResult is the sum so far. nextItem is the next value
-          let (operatorIndex,value) = nextItem
-          let operation = getOperator(comboToTest[operatorIndex])
-          return operation(partialResult,value)
-        }
-        if testResult == result { return result }
-      }
-      return nil
-    }.reduce(0, + )
+    
+    let (firstPassResult, failedEquations) = processEquations(equations, includeAll: false)
+    print("done with the first pass. Need to reprocess \(failedEquations.count)")
+    
+    
+    let (secondPassResult, _) = processEquations(failedEquations, includeAll: true)
+    
+    return firstPassResult + secondPassResult
   }
 }
 
