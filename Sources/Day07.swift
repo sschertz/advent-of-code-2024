48daf7eb212c42c5db1caaca0549e5d37a69e2a3
Sara Schertz <sschertz@me.com>
2024-12-18 18:16:14 -0700

    Completely redid all of day 7 / both parts
    No longer generating all the operator permutations up front.
    Use a recursive function to find equations that are valid

--- Sources/Day07.swift
+++ Sources/Day07.swift
@@ -6,126 +6,122 @@ struct Day07: AdventDay {
 
   // Splits input data into its component parts and convert from string.
   var entities: [Equation] {
-    data.split(separator: "\n").map{$0.split(separator: ":")}.map{
+    data.split(separator: "\n").map { $0.split(separator: ":") }.map {
       let result = Int($0[0])
-      let values = $0[1].split(separator: " ").compactMap{Int($0)}
+      let values = $0[1].split(separator: " ").compactMap { Int($0) }
       return Equation(result: result!, values: values)
     }
   }
-  
-  struct Equation {
-    var result: Int
-    var values: [Int]
-    var numberOperators: Int {
-      return values.count-1
-    }
-    var operators:[Character] {
-      Array(repeating: "+", count: numberOperators )
-      + Array(repeating: "*", count: numberOperators)
-    }
-    
-    var operatorsWithConcat: [Character] {
-      Array(repeating: "+", count: numberOperators )
-      + Array(repeating: "*", count: numberOperators)
-      + Array(repeating: "|", count: numberOperators)
-    }
-    
-    var basicOperatorCombos: [[Character]] {
-      operators.uniquePermutations(ofCount:numberOperators).map{Array($0)}
-    }
-    
-    var concatOperatorCombos: [[Character]]{
-      let basicOperatorCombos = Set(basicOperatorCombos)
-      let all = Set(operatorsWithConcat.uniquePermutations(ofCount: numberOperators).map{Array($0)})
-      return Array(all.subtracting(basicOperatorCombos))
-    }
-    
-    // might nto need this anymore?
-    func getOperatorPermutations(includeAll: Bool = false) -> [[Character]]{
-      let operators = (includeAll ? self.operatorsWithConcat : self.operators)
-      return operators.uniquePermutations(ofCount:numberOperators).map{Array($0)}
-    }
-  }
 
-  func getOperator (_ input: Character) -> (Int,Int) -> Int {
-    if input == "+" {
-      return (+)
-    } else if input == "*" { return (*)
-    } else { return (||) }
-  }
-  
-  func processEquations (_ equations:[Equation], includeAll: Bool = false) -> (Int,[Equation]) {
-    var failedEquations: [Equation] = []
-    
-    let result = equations.compactMap { equation in
-      var values = equation.values
-      print("solving \(equation.result)")
-      
-      let combinationsToTry: [[Character]] =
-      includeAll ? equation.concatOperatorCombos : equation.basicOperatorCombos
-      
-      let runningTotal = values.removeFirst()
-      
-      for comboToTest in combinationsToTry {
-        let testResult = values.enumerated().reduce(runningTotal) { partialResult, nextItem in
-          //partialResult is the sum so far. nextItem is the next value
-          let (operatorIndex,value) = nextItem
-          let operation = getOperator(comboToTest[operatorIndex])
-          return operation(partialResult,value)
-        }
-        if testResult == equation.result { return equation.result }
+  // Attempt to combine the previous result with the next value
+  // in the array using the operators. 
+  func evaluate(_ previousResult: inout Int, boundedBy: Int, remaining: [Int],
+    operations: [Operator]
+  ) -> Bool {
+
+    guard !remaining.isEmpty, previousResult <= boundedBy else {
+      return previousResult == boundedBy
+    }
+    let nextValue = remaining.first!
+    let nextRemaining = Array(remaining.dropFirst())
+    for operation in operations {
+      let op = operation.operation
+      var result = op(previousResult, nextValue)
+      if evaluate(
+        &result, boundedBy: boundedBy, remaining: nextRemaining,
+        operations: operations)
+      {
+        return true
       }
-      failedEquations.append(equation)
-      return nil
-    }.reduce(0, + )
-    
-    return (result,failedEquations)
-    
+    }
+    return false
   }
 
-  
   func part1() -> Any {
     let equations = entities
-    return equations.compactMap { equation in
-      let result = equation.result
-      var values = equation.values
-      let combinationsToTry = equation.operators.uniquePermutations(ofCount: equation.numberOperators).map{Array($0)}
-      // print("Find operators to get \(result) from \(values)")
 
-      // Get the first number to start the reduce
-      let runningTotal = values.removeFirst()
-      
-      // Loop all the combos, bail out when we find a match
-      for comboToTest in combinationsToTry {
-        let testResult = values.enumerated().reduce(runningTotal) { partialResult, nextItem in
-          //partialResult is the sum so far. nextItem is the next value
-          let (operatorIndex,value) = nextItem
-          let operation = getOperator(comboToTest[operatorIndex])
-          return operation(partialResult,value)
-        }
-        if testResult == result { return result }
+    let partOneOperators = [Operator.plus, Operator.mul]
+    print("Solve with operators: \(partOneOperators)")
+
+    return equations.compactMap { equation in
+      let expectedResult = equation.result
+      let remainingValues = Array(equation.values.dropFirst())
+      var startValue = equation.values.first!
+
+      if evaluate(
+        &startValue, boundedBy: expectedResult, remaining: remainingValues,
+        operations: partOneOperators)
+      {
+        return equation.result
+      } else {
+        return nil
       }
-      return nil
-    }.reduce(0, + )
+    }.reduce(0, +)
   }
 
   func part2() -> Any {
-    
     let equations = entities
-    
-    let (firstPassResult, failedEquations) = processEquations(equations, includeAll: false)
-    print("done with the first pass. Need to reprocess \(failedEquations.count)")
-    
-    
-    let (secondPassResult, _) = processEquations(failedEquations, includeAll: true)
-    
-    return firstPassResult + secondPassResult
+    let partTwoOperators = Operator.allCases
+    print("Solve with operators: \(partTwoOperators)")
+
+    return equations.compactMap { equation in
+      let expectedResult = equation.result
+      let remainingValues = Array(equation.values.dropFirst())
+      var startValue = equation.values.first!
+
+      if evaluate(
+        &startValue, boundedBy: expectedResult, remaining: remainingValues,
+        operations: partTwoOperators)
+      {
+        return equation.result
+      } else {
+        return nil
+      }
+    }.reduce(0, +)
+
   }
 }
 
 infix operator ||
+
 extension Int {
   static func || (lhs: Int, rhs: Int) -> Int {
     return Int(String(lhs) + String(rhs))!
+  }
+}
+
+struct Equation: CustomStringConvertible {
+  var result: Int
+  var values: [Int]
+
+  public var description: String {
+    return "\(result): \(values)"
+  }
+}
+
+public enum Operator: CaseIterable, CustomStringConvertible {
+
+  public var description: String {
+    switch self {
+    case .plus:
+      return "+"
+    case .mul:
+      return "*"
+    case .concat:
+      return "||"
+    }
+  }
+
+  case plus, mul, concat
+
+  public var operation: (Int, Int) -> Int {
+    switch self {
+    case .plus:
+      return (+)
+    case .mul:
+      return (*)
+    case .concat:
+      return (||)
+    }
   }
 }
